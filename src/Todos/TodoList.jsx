e1ebe18ab4fe42e778175066897ee35b512d0b02
Chirayu <tenguriachirayu@gmail.com>
2024-10-22 17:58:01 +0530

    secondary commit

--- src/Todos/TodoList.jsx
+++ src/Todos/TodoList.jsx
@@ -18,22 +18,5 @@ export default TodoList;
 
 
 
-// const TodoList = ({ task, handleTaskDelete }) => {
-//   return (
-//     <section className='myUnOrderList'>
-//       <ul>
-//         {task.map((currTask, index) => (
-//           <li key={index} className="todo-item">
-//             <span>{currTask}</span>
-//             <button className='check-btn'>
-//               <MdCheck />
-//             </button>
-//             <button className='delete-btn' onClick={() => handleTaskDelete(currTask)}>
-//               <MdDeleteForever />
-//             </button>
-//           </li>
-//         ))}
-//       </ul>
-//     </section>
-//   );
-// };
+
+
